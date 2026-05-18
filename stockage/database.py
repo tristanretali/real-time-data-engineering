d4335f9b0b61fee9b51ef0fbe66be90a99a18502
HAJJI <chaimaehajji999@gmail.com>
2026-05-18 14:34:25 +0200

    connexion mongo db

---
+++ stockage/database.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
+
+db = client["mongo-consumer"]
+
+print("MongoDB connecté !")
